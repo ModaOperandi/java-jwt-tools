e34053dee3ec3816cd04eaeec773fd3fd5f74e4a
YangCaoModa <yang.cao@modaoperandi.com>
2019-04-02 15:34:16 -0400

    Updated build.gradle.

--- src/main/java/com/modaoperandi/jwt/java/DecodedToken.java
+++ src/main/java/com/modaoperandi/jwt/java/DecodedToken.java
@@ -13,25 +13,25 @@ public class DecodedToken {
 	private String scopes;
 	private String[] groups;
 	private String[] roles;
-    private String[] permissions;
+	private String[] permissions;
     
-    public DecodedToken() {
-    	this.authToken = null;
-    	this.scopes = null;
-    	this.groups = null;
-    	this.roles = null;
-    	this.permissions = null;
-    }
+	public DecodedToken() {
+		this.authToken = null;
+		this.scopes = null;
+		this.groups = null;
+		this.roles = null;
+		this.permissions = null;
+	}
     
-    public DecodedToken(HttpServletRequest request) {
-    	helper(request);
-    	decodeClaims(this.authToken);
-    }
+	public DecodedToken(HttpServletRequest request) {
+		helper(request);
+		decodeClaims(this.authToken);
+	}
     
-    public DecodedToken(String token) {
-    	this.authToken = token;
-    	decodeClaims(token);
-    }
+	public DecodedToken(String token) {
+		this.authToken = token;
+		decodeClaims(token);
+	}
     
 	public String getToken() {
 		return this.authToken;
@@ -64,23 +64,23 @@ public class DecodedToken {
 	}
 	
 	public void printGroups() {
-	    System.out.println("Groups: " + arrayToString(this.groups));
+		System.out.println("Groups: " + arrayToString(this.groups));
 	}
 	
 	public void printRoles() {
-	    System.out.println("Roles: " + arrayToString(this.roles));
+		System.out.println("Roles: " + arrayToString(this.roles));
 	}
 	
 	public void printPermissions() {
-	    System.out.println("Permissions: " + arrayToString(this.permissions));
+		System.out.println("Permissions: " + arrayToString(this.permissions));
 	}
 	
 	public void printUserInfo() {
 		try {
-		    printScopes();
-		    printGroups();
-		    printRoles();
-		    printPermissions();
+			printScopes();
+			printGroups();
+			printRoles();
+			printPermissions();
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
@@ -96,24 +96,24 @@ public class DecodedToken {
 	}
 	
 	private void decodeClaims(String token) {	
-    	try {
-    	    DecodedJWT jwt = JWT.decode(token);
-    	    this.scopes = jwt.getClaim("scope").asString();
-    	    this.groups = jwt.getClaim(namespace + "groups").asArray(String.class);
-    	    this.roles = jwt.getClaim(namespace + "roles").asArray(String.class);
-    	    this.permissions = jwt.getClaim(namespace + "permissions").asArray(String.class);	    
-    	} catch (NullPointerException e) {
-    		System.out.println("Token is null.");
-    	} catch (JWTDecodeException e){
-    		System.out.println("Invalid token.");
-    	}
+		try {
+			DecodedJWT jwt = JWT.decode(token);
+			this.scopes = jwt.getClaim("scope").asString();
+			this.groups = jwt.getClaim(namespace + "groups").asArray(String.class);
+			this.roles = jwt.getClaim(namespace + "roles").asArray(String.class);
+			this.permissions = jwt.getClaim(namespace + "permissions").asArray(String.class);	   
+		} catch (NullPointerException e) {
+			System.out.println("Token is null.");
+		} catch (JWTDecodeException e){
+			System.out.println("Invalid token.");
+		}
 	}
 	
 	private String arrayToString(String[] array) {
-	    StringBuilder sb = new StringBuilder();
-	    for (String str : array) {
-	    	sb.append(str).append(" ");
-	    }
-	    return sb.toString().trim();
+		StringBuilder sb = new StringBuilder();
+		for (String str : array) {
+			sb.append(str).append(" ");
+		}
+		return sb.toString().trim();
 	}
 }
